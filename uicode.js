8967497d7f285576a2f16839b18a48bb418467ea
dima13230 <yandi512@yandex.ru>
2019-07-29 16:44:23 +0400

    Mobile adaptation №1

--- uicode.js
+++ uicode.js
@@ -1,8 +1,24 @@
+function detectmob() {
+   if(window.innerWidth <= 800 && window.innerHeight <= 600) {
+     return true;
+   } else {
+     return false;
+   }
+}
+
 var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
   lineNumbers: true,
   mode: "python"
 });
 editor.setSize(null, 400);
+
+//// Mobile adjust \\\\
+
+if (detectmob()) {
+	editor.getWrapperElement().style["font-size"] = 20+"px";
+}
+
+
 
 function new_f() {
 	editor.setValue("");
